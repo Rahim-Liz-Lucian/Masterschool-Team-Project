de03dad12bf0581bf311c364eb699c0ab12664bf
aboublef <kristopherab@gmail.com>
2022-11-12 01:14:43 +0000

    setup with styled components & preact

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,30 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import styled from "styled-components";
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+padding: 4em;
+background: papayawhip;
+width: 100vw;
+height: 100vh;
+display: flex;
+justify-content: center;
+text-align: center;
+`;
 
 export function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    // Use Title and Wrapper like any other React component – except they're styled!
+    return (
+        <Wrapper>
+            <Title>
+                Hello World!
+            </Title>
+        </Wrapper>
+    );
 }
