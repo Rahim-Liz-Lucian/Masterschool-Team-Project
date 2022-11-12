6cdfa3451a658aabf8e31ae107947f6e80fb3470
aboublef <kristopherab@gmail.com>
2022-11-12 09:01:18 +0000

    404 page

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { Link, Route } from "wouter-preact";
 import TypedPage from "./route/x/typed";
 import StyledPage from "./route/x/styled";
 // NOTE app starts here
+import NotFoundPage from "./route/404";
 import DashBoardPage from "./route/dashboard";
 import SignUpPage from "./route/sign-up";
 import SignInPage from "./route/sign-in";
@@ -22,6 +23,8 @@ export function App() {
             <Route path="/dashboard" component={DashBoardPage} />
             <Route path="/sign-in" component={SignInPage} />
             <Route path="/sign-up" component={SignUpPage} />
+
+            <Route path="/*" component={NotFoundPage} />
 
             <Route path="/x/typed" component={TypedPage} />
             <Route path="/x/styled" component={StyledPage} />
