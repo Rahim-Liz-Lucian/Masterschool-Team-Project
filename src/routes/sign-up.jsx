41d483a899790a833f9a04963d076de79af6527b
aboublef <kristopherab@gmail.com>
2022-11-12 03:24:45 +0000

    sign-up backend working

--- src/routes/sign-up.jsx
+++ src/routes/sign-up.jsx
@@ -1,5 +1,48 @@
+import { useRef } from "preact/hooks";
+import { useAuthContext } from "../firebase";
+
 export default function Page() {
+    const formRef = useRef();
+    const { user, authSignUp } = useAuthContext();
+
+    async function handleUserRegistration(e) {
+        e.preventDefault();
+
+        const formData = new FormData(formRef.current);
+
+        const email = formData.get("email");
+        const password = formData.get("password");
+
+        try {
+            await authSignUp(email, password);
+        } catch (error) {
+            console.error(`failed to create user: ${error.message}`);
+        }
+    }
+
     return (
-        <div>Hello</div>
+        <div>
+            <h1>Sign-up</h1>
+            {user && (
+                <div>
+                    <h3>Creation successful</h3>
+                    <p>Uid: {user.uid}</p>
+                    <p>Email: {user.email}</p>
+                </div>
+            )}
+
+            <form ref={formRef} onSubmit={handleUserRegistration}>
+                <label htmlFor="email">
+                    <span>Email:</span>
+                    <input required type="email" name="email" id="email" />
+                </label>
+                <label htmlFor="password">
+                    <span>Password:</span>
+                    <input required type="password" name="password" id="password" />
+                </label>
+                <button type="submit">Sign-up</button>
+            </form>
+
+        </div>
     );
 }
