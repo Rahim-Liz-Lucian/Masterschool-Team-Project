8d6b27bb5894f77b897588fbcc055fd17af0ea16
aboublef <kristopherab@gmail.com>
2022-11-12 04:24:49 +0000

    general cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,27 @@
 import { Route } from "wouter";
-import SignUpPage from "./routes/sign-up";
+// NOTE these are just for examples
 import TypedPage from "./routes/x/typed";
 import StyledPage from "./routes/x/styled";
+// NOTE app starts here
+import DashBoardPage from "./routes/dashboard";
+import SignUpPage from "./routes/sign-up";
+import SignInPage from "./routes/sign-in";
+import { authContext } from "./firebase";
 
 export function App() {
+    const AuthContext = authContext();
+
     return (
-        <>
+        <AuthContext>
             <Route path="/">
                 This is the home page
             </Route>
+            <Route path="/dashboard" component={DashBoardPage} />
+            <Route path="/sign-in" component={SignInPage} />
+            <Route path="/sign-up" component={SignUpPage} />
+
             <Route path="/x/typed" component={TypedPage} />
             <Route path="/x/styled" component={StyledPage} />
-            <Route path="/sign-up" component={SignUpPage} />
-        </>
+        </AuthContext>
     );
 }
