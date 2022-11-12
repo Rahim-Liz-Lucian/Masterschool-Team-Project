b92c76c081dd5d5393f1abe14bac46261699adb9
aboublef <kristopherab@gmail.com>
2022-11-12 01:32:24 +0000

    docs on resources used

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,16 @@
-import styled from "styled-components";
+import Index from "./routes";
+import { Link, Route } from "wouter";
 
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-font-size: 1.5em;
-text-align: center;
-color: palevioletred;
-`;
-
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-padding: 4em;
-background: papayawhip;
-width: 100vw;
-height: 100vh;
-display: flex;
-justify-content: center;
-text-align: center;
-`;
-
+// may need a stylesheet reset
 export function App() {
     // Use Title and Wrapper like any other React component – except they're styled!
     return (
-        <Wrapper>
-            <Title>
-                Hello World!
-            </Title>
-        </Wrapper>
+        // <Index></Index>
+        <>
+            <Route path="/" component={Index} />
+            <Route path="/users/:username">
+                {(params) => <div>Hello, {params.username}!</div>}
+            </Route>
+        </>
     );
 }
