8cd68f7d250026b680edf4e9facdad3914447c0a
Elizabeth Czarny <liz.cz91@gmail.com>
2022-11-15 10:13:01 +0100

    started implementing button component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,6 +4,8 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { db, auth } from "../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+import { ReactComponent as ReactLogo } from "../branding/Logo_with_text.svg";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -51,140 +53,97 @@ const SignUp = () => {
 
   return (
     <>
-      <section className="vh-100">
-        <div className="container h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-12 col-xl-11">
-              <div className="card text-black">
-                <div className="card-body p-md-5">
-                  <div className="row justify-content-center">
-                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                        Sign up
-                      </p>
-
-                      <form className="mx-1 mx-md-4">
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <input
-                              type="text"
-                              className="form-control"
-                              value={name}
-                              onChange={(e) => {
-                                setName(e.target.value);
-                              }}
-                            />
-                            <label className="form-label">Your Name</label>
-                          </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <input
-                              type="text"
-                              className="form-control"
-                              value={city}
-                              onChange={(e) => {
-                                setCity(e.target.value);
-                              }}
-                            />
-                            <label className="form-label">Your City</label>
-                          </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <input
-                              type="email"
-                              className="form-control"
-                              value={email}
-                              onChange={(e) => {
-                                setEmail(e.target.value);
-                              }}
-                            />
-                            <label className="form-label">Your Email</label>
-                          </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <input
-                              type="password"
-                              className="form-control"
-                              value={password}
-                              onChange={(e) => {
-                                setPassword(e.target.value);
-                              }}
-                            />
-                            <label className="form-label">Password</label>
-                          </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                          <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                          <div className="form-outline flex-fill mb-0">
-                            <input
-                              type="password"
-                              id="form3Example4cd"
-                              className="form-control"
-                              value={repeatPassword}
-                              onChange={(e) => {
-                                setRepeatPassword(e.target.value);
-                              }}
-                            />
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example4cd"
-                            >
-                              Repeat your password
-                            </label>
-                          </div>
-                        </div>
-
-                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                          <button
-                            type="button"
-                            className="btn btn-primary btn-lg"
-                            onClick={() => {
-                              if (
-                                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                                  email
-                                ) &&
-                                email &&
-                                name &&
-                                city &&
-                                password &&
-                                password === repeatPassword
-                              ) {
-                                handleSignUp();
-                              } else if (
-                                !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-                                  email
-                                )
-                              ) {
-                                window.alert("your email isnt valid");
-                              } else if (password !== repeatPassword) {
-                                window.alert("passwords arent the same");
-                              } else {
-                                window.alert("fill out everything");
-                              }
-                            }}
-                          >
-                            Register
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <section>
+        <ReactLogo
+          style={{ height: "160px", width: "100%", margin: "0 auto" }}
+        />
+        <h2 className="heading">Sign up to wasteless</h2>
+        <form className="mx-1 mx-md-4">
+          <div className="input-control">
+            <label className="form-label">Your Name</label>
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
           </div>
-        </div>
+
+          <div className="input-control">
+            <label className="form-label">Your Email</label>
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="input-control">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="input-control">
+            <label className="form-label" htmlFor="form3Example4cd">
+              Repeat your password
+            </label>
+            <input
+              type="password"
+              id="form3Example4cd"
+              className="form-control"
+              value={repeatPassword}
+              onChange={(e) => {
+                setRepeatPassword(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="input-control">
+            <label htmlFor="city" className="form-label">
+              Your City
+            </label>
+            <select
+              name="city"
+              id="city"
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+              required
+            >
+              <option value="none" disabled>
+                Select City
+              </option>
+              <option value="amsterdam">Amsterdam</option>
+              <option value="london">London</option>
+              <option value="berlin">Berlin</option>
+              <option value="paris">Paris</option>
+              <option value="tlv">Tel-Aviv</option>
+            </select>
+          </div>
+
+          <label htmlFor="terms">
+            <input type="checkbox" name="terms" required />I agree to the{" "}
+            <a href="/">Terms and conditions</a>
+          </label>
+          <label htmlFor="newsletter">
+            <input type="checkbox" name="newsletter" />
+            Sign me up to the newsletter
+          </label>
+          <Button text="Create Account" handleClick={handleSignUp} />
+        </form>
       </section>
     </>
   );
