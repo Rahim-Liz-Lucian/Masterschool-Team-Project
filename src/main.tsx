45abe13d0d8820fded8e588b687f57a15eaeefa9
Lucian Fuchs <fuchslucian@icloud.com>
2022-11-15 17:23:51 +0100

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { render } from 'preact';
-import { App } from './_app';
-// import { App } from './app';
+ import { App } from './app';
 // FIXME remove this
 import './index.css';
 
--- src/test/env.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe("env.test", () => {
-    test("read from env file", () => {
-        const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
-        expect(projectId).toBe("masterschool-team-project");
-    });
-});
