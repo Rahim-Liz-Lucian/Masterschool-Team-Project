79c618ca4cd87d8a479bab183ee55ae6e162a63c
aboublef <kristopherab@gmail.com>
2022-11-16 11:07:25 +0000

    updating users displayName and photoURL

--- src/route/settings/profile.jsx
+++ src/route/settings/profile.jsx
@@ -3,16 +3,18 @@ import SettingsProfileForm from "../../component/SettingsProfileForm";
 import { useFirebaseAuthData } from "../../firebase/hooks";
 import defaultAvatar from "../../assets/defaults/avatar.jpg";
 import { doc } from "firebase/firestore";
-import { fireStore, fireAuth } from "../../firebase";
+import { fireStore, fireAuth, fireStorage } from "../../firebase";
 import { useError } from "../../utils/hooks";
 import { updateCurrentUser, updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+import Compressor from "compressorjs";
 
 // NOTE using Github as a reference, it refreshes the page when changes are made
 // Is this behaviour we want or do we want it to update with no refresh. They also
 // keep this optional so can be blank.
 export default function Page() {
     const [currentUser, done] = useFirebaseAuthData();
-    const { update, unregister, error, resetError } = use({ currentUser });
+    const { update, updateAvatar, unregister, error, resetError } = use({ currentUser });
 
     if (!done) return (<div>Loading...</div>);
 
@@ -24,7 +26,7 @@ export default function Page() {
                 <label htmlFor="avatar">
                     <img src={currentUser?.photoURL ?? defaultAvatar} alt="avatar" onError={e => e.target.src = defaultAvatar} style={{ width: 75, borderRadius: 50, cursor: "pointer" }} />
                 </label>
-                <input type="file" id="avatar" accept="image/jpeg" style={{ display: "none" }} />
+                <input type="file" id="avatar" accept="image/jpeg" onChange={updateAvatar} style={{ display: "none" }} />
             </div>
 
             {/* TODO update user details form */}
@@ -40,6 +42,9 @@ const use = ({ currentUser }) => {
     const { error, setError, resetError } = useError();
 
     const update = async ({ name }) => {
+
+
+
         if (name) {
             // do something if name exists
             // TODO error handling
@@ -48,14 +53,46 @@ const use = ({ currentUser }) => {
         }
     };
 
-    const updatePhotoURL = () => {
-        try {
-            // updateProfile(auth, { photoURL: `` });
+    const updateAvatar = (e) => {
+        // get file, assumption that this exists given this event is only 
+        // triggered when there is a change
+        const rawFile = e.target.files[0];
 
-        } catch (error) {
-            setError(error);
-        }
+        new Compressor(rawFile, {
+            quality: 0.6, maxWidth: 1500, maxHeight: 1000,
+            error(error) { setError(error); },
+            async success(file) {
+                // TODO when the file has finished compressing, upload to server
+                const imgRef = ref(fireStorage, `users/${currentUser?.uid}/images`);
+                const task = uploadBytesResumable(imgRef, file, { contentType: file.type, customMetadata: {/* TODO */ } });
 
+                try {
+                    const _ = task.on("state_changed",
+                        snapshot => {
+                            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            console.log(`Upload is ${progress}% done`);
+                            switch (snapshot.state) {
+                                case "paused":
+                                    console.log("Upload is paused");
+                                    break;
+                                case "running":
+                                    console.log("Upload is running");
+                                    break;
+                            }
+                        },
+                        error => setError(error),
+                        async () => {
+                            const photoURL = await getDownloadURL(task.snapshot.ref);
+                            console.log('File available at', photoURL);
+
+                            await updateProfile(currentUser, { photoURL });
+                            alert(`Your profile picture has been updated`);
+                        });
+                } catch (error) {
+                    setError(error);
+                }
+            },
+        });
     };
 
     const unregister = (e) => {
@@ -72,5 +109,5 @@ const use = ({ currentUser }) => {
         }
     };
 
-    return { update, updatePhotoURL, unregister, error, resetError };
+    return { update, updateAvatar, unregister, error, resetError };
 };
