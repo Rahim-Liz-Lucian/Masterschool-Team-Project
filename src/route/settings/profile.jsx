f0a433bf3b559321d22d56a5d68b34725319bd1c
aboublef <kristopherab@gmail.com>
2022-11-24 12:49:06 +0000

    routes files will be updated

--- src/route/settings/profile.jsx
+++ src/route/settings/profile.jsx
@@ -12,83 +12,77 @@ import { useFirebaseAuth } from "~/firebase/hooks";
 // Is this behaviour we want or do we want it to update with no refresh. They also
 // keep this optional so can be blank.
 export default function Page() {
-  const [user, authLoading] = useFirebaseAuth();
-  const { error, resetError, updateAvatar, onUpdateProfile } = use({ user });
+    const [user, authLoading] = useFirebaseAuth();
+    const { error, resetError, updateAvatar, onUpdateProfile } = use({ user });
 
-  if (error) return <ErrorMessage {...{ error, resetError }} />;
+    if (error) return <ErrorMessage {...{ error, resetError }} />;
 
-  if (authLoading) return <div>Loading...</div>;
+    if (authLoading) return <div>Loading...</div>;
 
-  if (!user) return <Redirect to="/" />;
+    if (!user) return <Redirect to="/" />;
 
-  return (
-    <div>
-      <h1>Profile</h1>
-      {user.displayName && <h2>{user.displayName}'s Profile</h2>}
-      <p>{user.email}</p>
+    return (
+        <div>
+            <h1>Profile</h1>
+            {user.displayName && <h2>{user.displayName}'s Profile</h2>}
+            <p>{user.email}</p>
 
+            <div>
+                <label htmlFor="avatar">
+                    <img
+                        src={user?.photoURL ?? defaultAvatar}
+                        alt="avatar"
+                        onError={(e) => (e.target.src = defaultAvatar)}
+                        style={{ width: 75, borderRadius: 50, cursor: "pointer" }}
+                    />
+                </label>
+                <input
+                    type="file"
+                    id="avatar"
+                    accept="image/jpeg"
+                    onChange={updateAvatar}
+                    style={{ display: "none" }}
+                />
+            </div>
 
-      <div>
-        <label htmlFor="avatar">
-          <img
-            src={user?.photoURL ?? defaultAvatar}
-            alt="avatar"
-            onError={(e) => (e.target.src = defaultAvatar)}
-            style={{ width: 75, borderRadius: 50, cursor: "pointer" }}
-          />
-        </label>
-
-        <input
-          type="file"
-          id="avatar"
-          accept="image/jpeg"
-          onChange={updateAvatar}
-          style={{ display: "none" }}
-        />
-      </div>
-
-      {/* TODO update user details form */}
-      <SettingsProfileForm onUpdateProfile={onUpdateProfile} />
-
-      {/* have this in red */}
-
-      <DeleteAccountForm onDeleteAccount={onDeleteAccount} />
-    </div>
-  );
+            {/* TODO update user details form */}
+            <SettingsProfileForm onUpdateProfile={onUpdateProfile} />
+        </div>
+    );
 }
 
 const use = ({ user }) => {
-  const { error, setError, resetError } = useError();
-  const [, setLocation] = useLocation();
+    const { error, setError, resetError } = useError();
+    const [, setLocation] = useLocation();
 
-  const onUpdateProfile = async ({ name }) => {
-    // do something if name exists
-    // TODO error handling
-    await updateProfile(user, { displayName: name });
-    alert(`Your profile display name has been updated for ${name}`);
-  };
+    const onUpdateProfile = async ({ name }) => {
+        // do something if name exists
+        // TODO error handling
+        await updateProfile(user, { displayName: name });
+        alert(`Your profile display name has been updated for ${name}`);
+    };
 
-  const updateAvatar = (e) => {
-    new Compressor(e.target.files[0], {
-      quality: 0.6,
-      maxWidth: 1500,
-      maxHeight: 1000,
-      error(error) {
-        setError(error);
-      },
-      async success(file) {
-        try {
-          // file.name = "avatar";
-          // TODO return the url of the displayPhoto for this function
-          const photoURL = await uploadFile(file, `users/${user?.uid}/avatar`);
-          updateProfile(user, { photoURL });
-          alert(`Your profile picture has been updated 💚`); //NOTE to replace with a better message
-        } catch (error) {
-          setError(error);
-        }
-      },
-    });
-  };
+    const updateAvatar = (e) => {
+        new Compressor(e.target.files[0], {
+            quality: 0.6,
+            maxWidth: 1500,
+            maxHeight: 1000,
+            error(error) {
+                setError(error);
+            },
+            async success(file) {
+                try {
+                    // file.name = "avatar";
+                    // TODO return the url of the displayPhoto for this function
+                    const photoURL = await uploadFile(file, `users/${user?.uid}/avatar`);
+                    updateProfile(user, { photoURL });
+                    alert(`Your profile picture has been updated 💚`); //NOTE to replace with a better message
+                } catch (error) {
+                    setError(error);
+                }
+            },
+        });
+    };
 
-  return { error, resetError, updateAvatar, onUpdateProfile };
+    return { error, resetError, updateAvatar, onUpdateProfile };
 };
--- src/route/sign-in.jsx
+++ src/route/sign-in.jsx
@@ -1,14 +1,4 @@
 import { Link, useLocation } from "wouter-preact";
-<<<<<<< HEAD
-import { useFirebaseAuth } from "~/firebase/hooks";
-import { useError } from "~/utils/hooks";
-import wastelessLogo from "~/assets/brand/logo.svg";
-import SignInForm from "~/component/SignInForm";
-import { fireAuth } from "~/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import ErrorMessage from "~/component/base/ErrorMessage";
-import Button from "~/component/base/Button";
-=======
 import { useFirebaseAuth } from "../firebase/hooks";
 import { useError } from "../utils/hooks";
 import { WasteLess } from "../component/icons/icons";
@@ -21,70 +11,48 @@ import "../index.css";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { BsFacebook } from "react-icons/bs";
 import Back from "../component/base/Back";
->>>>>>> Liz_component_implementing
 
 export default function Page() {
-    const [, isLoading] = useFirebaseAuth();
+    // const [, isLoading] = useFirebaseAuth();
     const { authenticate, error, resetError } = use();
 
     // TODO if currentUser then redirect to browse
 
-<<<<<<< HEAD
     if (isLoading) return (
         <div>Loading...</div>
     );
-=======
-  return !error ? (
-    <div className="page">
-      <Back />
-      <WasteLess width="220px" />
->>>>>>> Liz_component_implementing
 
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-            <img src={wastelessLogo} alt="Waste-Less logo" style={{ height: "160px", width: "100%" }} />
-
-<<<<<<< HEAD
-            <SignInForm authenticate={authenticate} />
+            <img src={wastelessLogo} alt="Waste-Less logo" style={{ height: "160px", width: "100%" }} />;
 
             {/* FIXME Just needs center-ing */}
-            <Link href="/sign-up">Don't have an account yet? Sign up!</Link>
+            <p>
+                Don't have an account yet?
+                <Link href="/sign-up"> Sign up!</Link>
+            </p>
 
             {/* TODO sign in with google/facebook */}
-
+            <div>
+                <p>Or Sign Up using</p>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        marginTop: "5px",
+                        alignItems: "center",
+                    }}
+                >
+                    <AiFillGoogleCircle size="2.5rem" color="#de5246" />
+                    <BsFacebook size="2.2rem" color="#3b5998" />
+                </div>
+            </div>
             {/* TODO continue as guest */}
-            <Button classes="btn btn-secondary">Browse</Button>
+            <Button classes="btn btn-primary btn--border">Browse</Button>
         </div>
+    ) : (
+        <button onClick={resetError}>reset</button>
     );
-=======
-      {/* FIXME Just needs center-ing */}
-      <p>
-        Don't have an account yet?
-        <Link href="/sign-up"> Sign up!</Link>
-      </p>
-
-      {/* TODO sign in with google/facebook */}
-      <div>
-        <p>Or Sign Up using</p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            marginTop: "5px",
-            alignItems: "center",
-          }}
-        >
-          <AiFillGoogleCircle size="2.5rem" color="#de5246" />
-          <BsFacebook size="2.2rem" color="#3b5998" />
-        </div>
-      </div>
-      {/* TODO continue as guest */}
-      <Button classes="btn btn-primary btn--border">Browse</Button>
-    </div>
-  ) : (
-    <button onClick={resetError}>reset</button>
-  );
->>>>>>> Liz_component_implementing
 }
 
 const use = () => {
