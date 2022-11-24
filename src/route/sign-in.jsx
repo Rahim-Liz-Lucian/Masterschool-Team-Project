db430196a92651013602f2043af9e860edf76497
aboublef <kristopherab@gmail.com>
2022-11-24 15:30:55 +0000

    merging the sign-in page

--- src/route/sign-in.jsx
+++ src/route/sign-in.jsx
@@ -1,29 +1,49 @@
+import "./index.css";
 import { Link, useLocation } from "wouter-preact";
-import { useFirebaseAuth } from "../firebase/hooks";
 import { useError } from "../utils/hooks";
 import { WasteLess } from "../component/icons/icons";
 import SignInForm from "../component/SignInForm";
 import { fireAuth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import ErrorMessage from "../component/base/ErrorMessage";
-import Button from "../component/base/Button";
-import "../index.css";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { BsFacebook } from "react-icons/bs";
-import Back from "../component/base/Back";
+import Button, { BackButton } from "~/component/base/button";
+import ErrorMessage from "~/component/base/ErrorMessage";
+import { Input } from "~/component/base/input";
+import { useState } from "preact/hooks";
+import { signInUser } from "~/firebase/functions";
 
 export default function Page() {
     // const [auth, isLoaded] = useFirebaseAuthData();
-    const { authenticate, error, resetError } = use();
+    const [formData, setFormData] = useState({});
+    const { onSignIn, error, resetError } = useHook({ formData });
+
+    const onChange = (key) => e => {
+        setFormData({ ...formData, [key]: e.target.value });
+    };
 
     // TODO if currentUser then redirect to browse
+    if (error) return <ErrorMessage {...{ error, resetError }} />;
 
-    return !error ? (
+    return (
         <div className="page">
-            <Back />
+            <BackButton />
             <WasteLess width="220px" />
 
-            <SignInForm authenticate={authenticate} />
+            <form id="sign-in" onSubmit={onSignIn}>
+                <Input required name="email" type="email" value={formData.email} onChange={onChange("email")}>
+                    Email
+                </Input>
+                <Input required name="password" type="password" value={formData.password} onChange={onChange("password")} >
+                    Password
+                </Input>
+
+                <a href="/forgot-password">Forgot Password?</a>
+
+                <Button className="btn btn-primary" type="submit">
+                    Login
+                </Button>
+            </form>
 
             {/* FIXME Just needs center-ing */}
             <p>
@@ -47,26 +67,25 @@ export default function Page() {
                 </div>
             </div>
             {/* TODO continue as guest */}
-            <Button classes="btn btn-primary btn--border">Browse</Button>
         </div>
-    ) : (
-        <button onClick={resetError}>reset</button>
     );
 }
 
-const use = () => {
+const useHook = ({ formData: { email, password } }) => {
     const { error, setError, resetError } = useError();
+    const [, setLocation] = useLocation();
 
-    const authenticate = async ({ email, password }) => {
-        console.log(email, password);
+    const onSignIn = async (e) => {
+        e.preventDefault();
         try {
-            const cred = await signInWithEmailAndPassword(fireAuth, email, password);
+            const { user } = await signInUser(email, password);
             // should redirect but for now will just alert the user
-            alert(`Sign-in has been successful ${cred.user.uid} 💚`);
+            alert(`Sign-in has been successful ${user.uid} 💚`);
+            setLocation("/upload");
         } catch (error) {
             setError(error);
         }
     };
 
-    return { authenticate, error, resetError };
+    return { onSignIn, error, resetError };
 };
