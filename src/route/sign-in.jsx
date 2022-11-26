dfbe1522946618a4b795a6005f94521aa6baf162
aboublef <kristopherab@gmail.com>
2022-11-26 16:10:39 +0000

    refactoring sign-up page

--- src/route/sign-in.jsx
+++ src/route/sign-in.jsx
@@ -1,17 +1,10 @@
-import "./index.css";
+import "./sign-in.css";
 import { Link, useLocation } from "wouter-preact";
-import { useError } from "../utils/hooks";
-import { WasteLess } from "../component/icons/icons";
-import SignInForm from "../component/SignInForm";
-import { fireAuth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
-import Button, { BackButton } from "~/component/base/button";
-import ErrorMessage from "~/component/base/ErrorMessage";
-import { Input } from "~/component/base/input";
+import { useError } from "~/utils";
+import { WasteLess, Input, BackButton, Google, Facebook } from "~/component/core";
+import ErrorMessage from "~/component/ErrorMessage";
 import { useState } from "preact/hooks";
-import { signInUser } from "~/firebase/functions";
+import { signInUser } from "~/firebase";
 
 export default function Page() {
     // const [auth, isLoaded] = useFirebaseAuthData();
@@ -26,48 +19,35 @@ export default function Page() {
     if (error) return <ErrorMessage {...{ error, resetError }} />;
 
     return (
-        <div className="page">
-            <BackButton />
-            <WasteLess width="220px" />
+        <>
+            <header className="header">
+                <BackButton />
+            </header>
 
-            <form id="sign-in" onSubmit={onSignIn}>
-                <Input required name="email" type="email" value={formData.email} onChange={onChange("email")}>
-                    Email
-                </Input>
-                <Input required name="password" type="password" value={formData.password} onChange={onChange("password")} >
-                    Password
-                </Input>
+            <main>
+                {/* TODO move logo to header */}
+                <WasteLess width={220} />
 
-                <a href="/forgot-password">Forgot Password?</a>
+                <form className="form" id="sign-in" onSubmit={onSignIn}>
+                    <Input name="email" type="email" value={formData.email} onChange={onChange("email")}>Email</Input>
 
-                <Button className="btn btn-primary" type="submit">
-                    Login
-                </Button>
-            </form>
+                    <Input name="password" type="password" value={formData.password} onChange={onChange("password")}>Password</Input>
 
-            {/* FIXME Just needs center-ing */}
-            <p>
-                Don't have an account yet?
-                <Link href="/sign-up"> Sign up!</Link>
-            </p>
+                    <a className="form__reset" href="/forgot-password">Forgot Password?</a>
 
-            {/* TODO sign in with google/facebook */}
-            <div>
-                <p>Or Sign Up using</p>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                        marginTop: "5px",
-                        alignItems: "center",
-                    }}
-                >
-                    <AiFillGoogleCircle size="2.5rem" color="#de5246" />
-                    <BsFacebook size="2.2rem" color="#3b5998" />
-                </div>
-            </div>
-            {/* TODO continue as guest */}
-        </div>
+                    <button className="form__submit" type="submit">Login</button>
+
+                    <span>Don't have an account yet?<Link href="/sign-up"> Sign up!</Link></span>
+
+                    {/* TODO sign in with google/facebook */}
+                    <div className="oauth">
+                        <p>Or Sign Up using</p>
+                        <Google className="icon--google" />
+                        <Facebook className="icon--facebook" />
+                    </div>
+                </form>
+            </main>
+        </>
     );
 }
 
@@ -81,7 +61,7 @@ const useHook = ({ formData: { email, password } }) => {
             const { user } = await signInUser(email, password);
             // should redirect but for now will just alert the user
             alert(`Sign-in has been successful ${user.uid} 💚`);
-            setLocation("/upload");
+            setLocation("/");
         } catch (error) {
             setError(error);
         }
