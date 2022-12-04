252c73dbaf5dcc1a45194887e9a564e1ecedfff9
Elizabeth Czarny <liz.cz91@gmail.com>
2022-12-04 18:14:50 +0100

    feat[[id]]: passed information to product page, added styling, fixed button styling

--- src/component/NavMenu.jsx
+++ src/component/NavMenu.jsx
@@ -2,29 +2,42 @@ import { Link, useLocation } from "wouter-preact";
 import { Add, Home, Message, More, Profile } from "./core";
 
 const NavMenu = ({ user }) => {
-    const [location] = useLocation();
+  const [location] = useLocation();
 
-    if (!user) return (
-        <div className="nav--main">
-            <Link className="button" to="/sign-in">Login</Link>
-            <Link className="button--secondary" to="/sign-up">Sign Up</Link>
-        </div>
+  if (!user)
+    return (
+      <div className="nav--main">
+        <Link className="button" to="/sign-in">
+          Login
+        </Link>
+        <Link className="button--secondary" to="/sign-up">
+          Sign Up
+        </Link>
+      </div>
     );
 
-    const className = "nav__icon";
+  const className = "nav__icon";
 
-    linksSync.forEach(link => {
-        console.log({ path: link.path, bool: link.path === "/" + window.location.pathname.split`/`[1] });
-    });
+  linksSync.forEach((link) => {
+    // console.log({ path: link.path, bool: link.path === "/" + window.location.pathname.split`/`[1] });
+  });
 
-    return (
-        <nav className="nav--main">
-            {linksSync.map(link => ({ ...link, className: "/" + window.location.pathname.split`/`[1] === link.path ? "nav__icon--active" : "nav__icon" })).map(({ path, Component, className }) => (
-                <Link to={path}>
-                    <Component className={className} />
-                </Link>
-            ))}
-            {/* <Link to="/settings/">
+  return (
+    <nav className="nav--main">
+      {linksSync
+        .map((link) => ({
+          ...link,
+          className:
+            "/" + window.location.pathname.split`/`[1] === link.path
+              ? "nav__icon--active"
+              : "nav__icon",
+        }))
+        .map(({ path, Component, className }) => (
+          <Link to={path}>
+            <Component className={className} />
+          </Link>
+        ))}
+      {/* <Link to="/settings/">
                 <Profile className={[className, undefined].join`--`} />
             </Link>
             <Link to="/upload">
@@ -39,20 +52,26 @@ const NavMenu = ({ user }) => {
             <Link to="/misc">
                 <More className={className} />
             </Link> */}
-        </nav>
-    );
+    </nav>
+  );
 };
 
 const linksSync = [
-    { path: "/settings", Component: Profile },
-    { path: "/upload", Component: Add },
-    { path: "/", Component: Home },
-    { path: "/people", Component: Message },
-    { path: "/misc", Component: More },
+  { path: "/settings", Component: Profile },
+  { path: "/upload", Component: Add },
+  { path: "/", Component: Home },
+  { path: "/people", Component: Message },
+  { path: "/misc", Component: More },
 ];
 
 const activeLink = () => {
-    return linksSync.map(link => ({ ...link, className: window.location.pathname.split`/`[1] === link.path ? "nav__icon--active" : "nav__icon" }));
+  return linksSync.map((link) => ({
+    ...link,
+    className:
+      window.location.pathname.split`/`[1] === link.path
+        ? "nav__icon--active"
+        : "nav__icon",
+  }));
 };
 
 export default NavMenu;
--- src/route/products/[id].jsx
+++ src/route/products/[id].jsx
@@ -1,102 +1,122 @@
 import avatarFallback from "../../assets/brand/avatar-fallback.jpg";
-
+import "./[id].css";
 import { Link } from "wouter-preact";
 import ErrorMessage from "~/component/ErrorMessage";
 import { useFireBaseAuth, useFirebaseProductByID } from "~/firebase";
-import { useError } from "~/utils";
-import { Star, Share, Telephone, Person, BackButton, Button, LinkAvatar, Avatar, WasteLessLite } from "~/component/core";
+import { useError, relativeDays } from "~/utils";
+import {
+  Star,
+  Share,
+  Telephone,
+  Person,
+  BackButton,
+  Button,
+  LinkAvatar,
+  Avatar,
+  WasteLessLite,
+} from "~/component/core";
 import NavMenu from "~/component/NavMenu";
 
-
 export default function ProductPage({ id, ...props }) {
-    const user = useFireBaseAuth();
+  const user = useFireBaseAuth();
 
-    const { error, resetError, product, loading: dataLoading } = useHook({ user, id });
+  const {
+    error,
+    resetError,
+    product,
+    loading: dataLoading,
+  } = useHook({ user, id });
+  console.log(product);
 
-    const thumbnailURL = product?.thumbnailURL ?? avatarFallback;
+  const thumbnailURL = product?.thumbnailURL ?? avatarFallback;
+  const expirationDate = product?.expirationDate
+    ? product.expirationDate.toLocaleDateString()
+    : null;
+  const createdAt = product?.createdAt
+    ? relativeDays(product.createdAt.getTime())
+    : null;
 
-    if (error) return <ErrorMessage {...{ error, resetError }} />;
+  if (error) return <ErrorMessage {...{ error, resetError }} />;
 
-    if (!user) return (
-        <>
-            <header className="header">
-                <BackButton />
-                <h1 className="header__title">Details</h1>
-                <WasteLessLite className="icon" />
-            </header>
-
-            <main>
-                <h1>Hi! you must be signed in to be here</h1>
-                <div className="buttons">
-                    <Link to="/sign-up">
-                        <Button className="btn btn-primary">Sign Up</Button>
-                    </Link>
-                    <Link to="/sign-in" preload>
-                        <Button className="btn btn-primary btn--border">Sign In</Button>
-                    </Link>
-                </div>
-            </main>
-
-            <NavMenu user={user} />
-        </>
-    );
-
-    if (dataLoading) return <div>Data Loading...</div>;
-
-    // TODO 404 component
-    if (!product) return (<div>404 Product does not exist</div>);
-
+  if (!user)
     return (
-        <>
-            <header className="header">
-                <BackButton />
-                <h1 className="header__title">Details</h1>
-                <WasteLessLite className="icon" />
-            </header>
+      <>
+        <header className="header">
+          <BackButton />
+          <h1 className="header__title">Details</h1>
+          <WasteLessLite className="icon" />
+        </header>
 
-            <main className="product__page">
-                <h2 className="product__title">product Title</h2>
-                <img className="product__thumbnail" src={thumbnailURL} alt="" />
-                <div>
-                    <p>location</p>
-                    <p>date</p>
-                    <p>days ago</p>
-                </div>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-                    et exercitationem nisi quae officia tempora magni explicabo.
-                </p>
-                <div>
-                    <Person size={"2rem"} />
-                    <h3>User Name</h3>
-                    <Star />
-                    <Star />
-                    <Star />
-                    <Star />
-                </div>
-                <div className="buttons">
-                    <Button classes="btn btn-primary">
-                        <Telephone />
-                        Call
-                    </Button>
-                    <Button classes="btn btn-primary btn--border">
-                        <Share />
-                        Share
-                    </Button>
-                </div>
+        <main>
+          <h1>Hi! you must be signed in to be here</h1>
+          <div className="buttons">
+            <Link to="/sign-up">
+              <Button className="btn btn-primary">Sign Up</Button>
+            </Link>
+            <Link to="/sign-in" preload>
+              <Button className="btn btn-primary btn--border">Sign In</Button>
+            </Link>
+          </div>
+        </main>
 
-            </main>
-
-            <aside>
-                <NavMenu user={user} />
-            </aside>
-        </>
+        <NavMenu user={user} />
+      </>
     );
+
+  if (dataLoading) return <div>Data Loading...</div>;
+
+  // TODO 404 component
+  if (!product) return <div>404 Product does not exist</div>;
+
+  return (
+    <>
+      <header className="header">
+        <BackButton />
+        <h1 className="header__title">Details</h1>
+        <WasteLessLite className="icon" />
+      </header>
+
+      <main className="product__page">
+        <h2 className="product__title">{product.title}</h2>
+        <img className="product__thumbnail" src={thumbnailURL} alt="" />
+        <div className="product__details">
+          <p>{product.user.location.city}</p>
+          <p>{expirationDate}</p>
+          <p>{createdAt}</p>
+        </div>
+        <p>{product.description}</p>
+        <div className="product__user">
+          <Person size={"1.5rem"} />
+          <h3>{product.user.displayName}</h3>
+          <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </div>
+        </div>
+        <div className="buttons">
+          <Button classes="button">
+            <Telephone />
+            Call
+          </Button>
+          <Button classes="button-secondary">
+            <Share />
+            Share
+          </Button>
+        </div>
+      </main>
+
+      <aside>
+        <NavMenu user={user} />
+      </aside>
+    </>
+  );
 }
 
 const useHook = ({ id }) => {
-    const { product, error: productError, loading } = useFirebaseProductByID(id);
-    const { error, resetError } = useError(productError);
+  const { product, error: productError, loading } = useFirebaseProductByID(id);
+  const { error, resetError } = useError(productError);
 
-    return { error, resetError, loading, product, owner: product?.user };
+  return { error, resetError, loading, product, owner: product?.user };
 };
